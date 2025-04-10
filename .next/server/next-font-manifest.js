c43778f3e7df7a1d6fd4215fb27af1827803bb11
sanusimon <saanusimon91@gmail.com>
2025-04-10 21:50:39 +0530

    build

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
-=======
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/src/app/page\": [\n      \"static/media/92zatBhPNqw73oTd4jQmfxI-s.p.ba172912.woff2\"\n    ],\n    \"[project]/src/app/list/page\": [\n      \"static/media/92zatBhPNqw73oTd4jQmfxI-s.p.ba172912.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
->>>>>>> 8bb901ac (.nxt)
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/sanu/aureliah-fashion/src/app/layout":["static/media/9dd75fadc5b3df29-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"7fdfea2788976b66197baa1aa6199341fd49dd46f8\":{\"workers\":{\"app/orders/page\":{\"moduleId\":\"44224\",\"async\":false},\"app/profile/page\":{\"moduleId\":\"44224\",\"async\":false}},\"layer\":{\"app/orders/page\":\"rsc\",\"app/profile/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CFfXZBXAbgJorB8zCcf+mLX6P0p7xTEFGDoU0/wSVXc=\"\n}"
->>>>>>> 8bb901ac (.nxt)
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"7fc740b9a9a85a80aed9624dab78b219d0f882a1c8\":{\"workers\":{\"app/orders/page\":{\"moduleId\":\"91999\",\"async\":false},\"app/profile/page\":{\"moduleId\":\"91999\",\"async\":false}},\"layer\":{\"app/orders/page\":\"rsc\",\"app/profile/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
