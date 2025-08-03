c729a94ea9128b8610ec34bc29a6b3ee6914b698
neethurithesh <neethuka36@gmail.com>
2025-08-03 17:02:17 +0530

    Update useCartStore.jsx

--- src/hooks/useCartStore.jsx
+++ src/hooks/useCartStore.jsx
@@ -3,6 +3,7 @@ import { WixClient } from "@/Context/WixContext/WixContext";
 import Cookies from "js-cookie";
 import { currentCart } from "@wix/ecom"; 
  
+
 type CartState = {
   cart: currentCart.Cart;
   isLoading: boolean;
@@ -16,67 +17,56 @@ type CartState = {
   ) => void;
   removeItem: (wixClient: WixClient, itemId: string) => void;
 };
-    
 
-export const useCartStore = create((set) => ({
-    
-    cart:[],
-    isLoading:true,
-    counter:0,
+export const useCartStore = create<CartState>((set) => ({
+  cart: [],
+  isLoading: true,
+  counter: 0,
+  getCart: async (wixClient) => {
+    try {
+      const cart = await wixClient.currentCart.getCurrentCart();
+      set({
+        cart: cart || [],
+        isLoading: false,
+        counter: cart?.lineItems.length || 0,
+      });
+    } catch (err) {
+      set((prev) => ({ ...prev, isLoading: false }));
+    }
+  },
+  addItem: async (wixClient, productId, variantId, quantity) => {
+    set((state) => ({ ...state, isLoading: true }));
+    const response = await wixClient.currentCart.addToCurrentCart({
+      lineItems: [
+        {
+          catalogReference: {
+            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
+            catalogItemId: productId,
+            ...(variantId && { options: { variantId } }),
+          },
+          quantity: quantity,
+        },
+      ],
+    });
 
-     getCart: async (wixClient) => {
-      //  const refreshToken = Cookies.get('refreshToken'); // Ensure refreshToken exists in cookies
-         
-        /*if (!refreshToken) {
-            console.warn("No refresh token found. Reinitializing session...");
-            set({ cart: [], isLoading: false, counter: 0 });  // If no refresh token, reset the cart
-            return;
-        }*/
+    set({
+      cart: response.cart,
+      counter: response.cart?.lineItems.length,
+      isLoading: false,
+    });
+  },
+  removeItem: async (wixClient, itemId) => {
+    set((state) => ({ ...state, isLoading: true }));
+    const response = await wixClient.currentCart.removeLineItemsFromCurrentCart(
+      [itemId]
+    );
 
-        try {
-            // Fetch the current cart
-            const cart = await wixClient.currentCart.getCurrentCart();
-            set({ cart, isLoading: false, counter: cart?.lineItems?.length || 0 });
-        } catch (e) {
-            console.warn("Cart not found, creating new one implicitly...");
-            set({ cart: [], isLoading: false, counter: 0 });  // Handle failure to fetch cart
-        }
-    },
-
-
-            
-    addItem: async (wixClient, productId,variantId,quantity)=>{
-
-        set((state)=>({...state, isLoading:true}))
-        const response = await wixClient.currentCart.addToCurrentCart({
-            lineItems:[{
-                catalogReference:{
-                    appId:process.env.NEXT_PUBLIC_WIX_APP_ID,
-                    catalogItemId:productId,
-                    ...(variantId && {options:{variantId}})
-                },
-                quantity:quantity
-            }]
-        });
-        set({
-            cart: response.cart,
-            counter:response.cart?.lineItems.length,
-            isLoading:false
-        })
-
-    },
-    removeItem: async (wixClient,itemId)=>{
-        set((state)=>({...state, isLoading:true}))
-        const response = await wixClient.currentCart.removeLineItemsFromCurrentCart(
-            [itemId]
-          );
-        
-        set({
-            cart: response.cart,
-            counter:response.cart?.lineItems.length,
-            isLoading:false
-        })
-    },
+    set({
+      cart: response.cart,
+      counter: response.cart?.lineItems.length,
+      isLoading: false,
+    });
+  },
     updateQuantity: async (wixClient, itemId, newQuantity) => {
         set((state) => ({ ...state, isLoading: true }));
         const response = await wixClient.currentCart.updateCurrentCartLineItemQuantity([
