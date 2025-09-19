3148b04d5dd43fe9a47480b73cb3110bb40f168c
sanusimon <saanusimon91@gmail.com>
2025-09-19 09:44:04 +0530

    banner changes

--- src/Components/HomeBanner/HomeBanner.jsx
+++ src/Components/HomeBanner/HomeBanner.jsx
@@ -43,7 +43,7 @@ const HomeBanner = () => {
     { imgPath: "/banner3.jpg",bannerText:"Where Comfort Meets Elegance<br> <span> Top & Dupatta</span>", id: "img3", categorySlug: cats[5]?.slug || "all-products" },
     { imgPath: "/banner01.jpg", bannerText:"	Coord Sets That Define Effortless <br> <span> Style</span>", id: "img1", categorySlug: cats[1]?.slug || "all-products" },
     { imgPath: "/banner02.jpg",bannerText:"Kurtis for the Modern <br> <span>Muse</span>", id: "img2", categorySlug: cats[6]?.slug || "all-products" },
-    // { imgPath: "/banner4.jpg",bannerText:"Short Tops, Big Style  <br> <span>Statements</span>", id: "img4", categorySlug: cats[3]?.slug || "all-products" },
+    { imgPath: "/banner4.png",bannerText:"Discover our handpicked kurti <br> <span>collection</span>", id: "img4", categorySlug: cats[3]?.slug || "all-products" },
     // { imgPath: "/banner5.jpg",bannerText:"Sarees That Speak Grace and  <br> <span>Glamour</span>", id: "img5", categorySlug: cats[4]?.slug || "all-products" },
   ];
 
