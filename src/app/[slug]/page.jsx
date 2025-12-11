992ead2457e69e584c22db9e550ddab794aa20ba
sanusimon <saanusimon91@gmail.com>
2025-12-11 13:22:39 +0530

    d

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import './productDetail.scss';
 import { wixClientServer } from "@/lib/wixClientServer";
 import AddQuantity from '@/Components/AddQuantity/AddQuantity';
