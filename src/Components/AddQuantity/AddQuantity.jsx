a99e6e437a36307565426aa2d2ad0c12392c9d10
sanusimon <saanusimon91@gmail.com>
2026-03-23 18:14:00 +0530

    quantity limit changes

--- src/Components/AddQuantity/AddQuantity.jsx
+++ src/Components/AddQuantity/AddQuantity.jsx
@@ -45,8 +45,14 @@ function AddQuantity({productId,variantId,stockNumber , isSelected}) {
                     <p>{quantity}</p>
                 <label className='qnty_btn plus' onClick={() => handleQuantity("i")} disabled={quantity === stockNumber}>+</label>
                 </div>
-                {stockNumber < 1 ? (<span className='dont_miss'>Product is out of stock</span>) 
-                :(<span className='small_txt'>Only <span>  {stockNumber} items </span> left! <br />Don't Miss it</span>)} 
+                {/* Show stock details only if isSelected is true */}
+                {isSelected && (
+                  stockNumber < 1 ? (
+                    <span className='dont_miss'>Product is out of stock</span>
+                  ) : (
+                    <span className='small_txt'>Only <span>  {stockNumber} items </span> left! <br />Don't Miss it</span>
+                  )
+                )}
             </span>
            
             
@@ -69,4 +75,4 @@ function AddQuantity({productId,variantId,stockNumber , isSelected}) {
 }
 
 export default AddQuantity
-  
+
